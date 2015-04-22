bc673b36ec535fa095b6d547b32f1e24a5465427
Clelio De Souza <cleliofs@gmail.com>
2015-04-22 23:00:02 +0100

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,20 @@
+name := "solver-server"
+
+organization := "uk.co.codesynergy"
+
+version := "1.0"
+
+scalaVersion := "2.11.6"
+
+libraryDependencies ++= Seq(
+  "org.apache.camel" % "camel-scala" % "2.15.1",
+  "org.apache.activemq" % "activemq-core" % "5.7.0",
+  "org.apache.camel" % "camel-jms" % "2.15.1",
+  "ch.qos.logback" % "logback-core" % "1.1.3",
+  "ch.qos.logback" % "logback-classic" % "1.1.3",
+  "javax.mail" % "mail" % "1.4.7",
+  "com.icegreen" % "greenmail" % "1.4.0",
+  "org.apache.camel" % "camel-mail" % "2.15.1",
+  "org.apache.camel" % "camel-velocity" % "2.15.1"
+)
+    
---
+++ src/main/scala/uk.co.codesynergy/RoutingAfterCBRExample.scala
@@ -0,0 +1,47 @@
+package uk.co.codesynergy
+
+import javax.jms.ConnectionFactory
+
+import org.apache.activemq.ActiveMQConnectionFactory
+import org.apache.camel.{Exchange, CamelContext}
+import org.apache.camel.component.jms.JmsComponent
+import org.apache.camel.impl.DefaultCamelContext
+import org.apache.camel.scala.dsl.builder.{ScalaRouteBuilder, RouteBuilderSupport}
+
+/**
+ * Created by clelio on 22/04/15.
+ */
+object RoutingAfterCBRExample extends App with RouteBuilderSupport {
+  val context: CamelContext = new DefaultCamelContext
+  val connectionFactory: ConnectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616")
+  context.addComponent("jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory))
+  val routeBuilder = new ScalaRouteBuilder(context) {
+    from("file:src/test/resources/inbox")
+      .choice {
+        when(fileEndsWith(_, "xml")) --> ("jms:xmlOrders")
+        when(fileEndsWith(_, "csv")) --> ("jms:csvOrders")
+        otherwise() --> ("jms:unknownOrders")
+    }
+
+    from("jms:xmlOrders")
+      .process(exchange => println(s"XML type order received: ${exchange.getIn().getHeader("CamelFileName")}"))
+    from("jms:csvOrders")
+      .process(exchange => println(s"CSV type order received: ${exchange.getIn().getHeader("CamelFileName")}"))
+    from("jms:unknownOrders")
+      .process(exchange => println(s"Unknown type order received: ${exchange.getIn().getHeader("CamelFileName")}"))
+
+  }
+
+  context.addRoutes(routeBuilder)
+  context.start
+  while (true) {}
+
+  def fileEndsWith(ex: Exchange, fileExt: String): Boolean = {
+    ex.getIn.getHeader("CamelFileName") match {
+      case x: String => return x.endsWith(fileExt)
+      case _ => false
+    }
+  }
+
+
+}
